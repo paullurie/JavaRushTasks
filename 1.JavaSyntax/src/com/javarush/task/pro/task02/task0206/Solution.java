881ec53859c495d358008810341106013e884dd1
79313 <paul.lurie@yandex.ru>
2022-11-25 14:51:37 +0300

    Дабавил задачи со второго уровня

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Hello", phrase = "Hi, world", line = "name", text = "text";
+
+    }
+}
