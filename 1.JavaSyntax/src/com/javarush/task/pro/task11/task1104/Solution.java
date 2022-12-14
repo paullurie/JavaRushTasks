f4af448ae5bc5629cd57f270776ef16175c80b8b
79313 <paul.lurie@yandex.ru>
2022-12-14 16:25:50 +0300

    task 0605

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        double result = Double.parseDouble(string);
+        int result1 = (int)Math.round(result);
+        System.out.println(result1);
+    }
+}
